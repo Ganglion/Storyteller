52ae129a32c1a0f1e626e347e7bc84caede99a1e
Luke <leeluke_9@live.com>
2018-04-15 12:27:04 +0800

    - Underwater now looks awesome - Forest now looks awesome - Music for forest, factory, underwater and volcano. - Fixed bug where the boy could jump out of the cloud area - Fixed bug where the boy could jump out onto the surface of the volcano after the eruption

---
+++ Storyteller/Assets/TagSetter.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TagSetter : MonoBehaviour {
+
+    [SerializeField]
+    private GameObject objToSetTag;
+
+    private void OnTriggerEnter2D(Collider2D other) {
+        if (other.gameObject.layer == LayerMask.NameToLayer("Player")) {
+            SetBoundary(objToSetTag);
+        }
+    }
+
+    public void SetBoundary(GameObject obj) {
+        obj.tag = "Boundary";
+    }
+
+}
